7aaae707be1ce5237a140a238939f58be6f8c9a9
Aziz <aziz.abdurahimov151002@gmail.com>
2022-03-06 11:35:00 +0500

    lowercase service variables

--- controllers/AccountsController.js
+++ controllers/AccountsController.js
@@ -1,21 +1,21 @@
-const AccountsService = require('../services/AccountsService');
+const accountsService = require('../services/AccountsService');
 
 class AccountsController {
   create(req, res) {
     const newAcc = req.body;
 
-    if (AccountsService.create(newAcc)) res.json(newAcc);
+    if (accountsService.create(newAcc)) res.json(newAcc);
     else res.json({ message: 'account with this id already exists' });
   }
 
   getAll(req, res) {
-    const accounts = AccountsService.getAll();
+    const accounts = accountsService.getAll();
     res.send(accounts);
   }
 
   getOne(req, res) {
     const id = +req.params.id;
-    const account = AccountsService.getOne(id);
+    const account = accountsService.getOne(id);
 
     if (account) res.send(account);
     else res.status(404).json({ message: "user doesn't exist" });
@@ -25,14 +25,14 @@ class AccountsController {
     const account = req.body;
     account.id = +req.params.id;
 
-    if (AccountsService.update(account)) res.json(account);
+    if (accountsService.update(account)) res.json(account);
     else res.json({ message: "account doesn't exist" });
   }
 
   delete(req, res) {
     const id = +req.params.id;
 
-    if (AccountsService.delete(id)) res.json({ message: 'success' });
+    if (accountsService.delete(id)) res.json({ message: 'success' });
     else res.json({ message: "account doesn't exist" });
   }
 }
--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,20 +1,20 @@
-const UsersService = require('../services/UsersService');
+const usersService = require('../services/UsersService');
 
 class UsersController {
   create(req, res) {
     const newUser = req.body;
-    if (UsersService.create(newUser)) res.json(newUser);
+    if (usersService.create(newUser)) res.json(newUser);
     else res.json({ message: 'user with this id already exists' });
   }
 
   getAll(req, res) {
-    const users = UsersService.getAll();
+    const users = usersService.getAll();
     res.json(users);
   }
 
   getOne(req, res) {
     const id = +req.params.id;
-    const user = UsersService.getOne(id);
+    const user = usersService.getOne(id);
     if (user) res.json(user);
     else res.status(404).json({ message: "user doesn't exist" });
   }
